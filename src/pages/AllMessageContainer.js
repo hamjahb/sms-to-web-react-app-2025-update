6e926207c0fe6cd0aa422102d5c06088d2892911
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-02-15 14:03:42 +0300

    display message container

--- src/pages/AllMessageContainer.js
+++ src/pages/AllMessageContainer.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 
 
 class AllMessageContainer extends Component {
+    constructor(props){
+        super (props)
+        
+      }
+
+      render() {
+          return(
+              <h5>message container</h5>
+          )
+      }
 
 }
 
