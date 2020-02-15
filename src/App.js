5a0abcf5c7743d1b2d61910b4cf4080cf8420442
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-02-15 13:55:40 +0300

    add on click for button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-      <head>
-        <title>Hisham Code</title>
-      </head>
 
-      <header className="App-header">
-        <p>this page shows any sms messages recived by hisham</p>
-      </header>
-
-      <body className="App-body">
-        <h1>here will lie all text containers</h1>
-        <smsContainer/>
-        <button>Check for New Code</button>
-      </body>
-
-      <footer className="App-footer">
-        <p>created by Hisham Aljahbli</p>
-      </footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super (props)
+    
+    this.state = {
+      smsList: []
+    }
+  }
+  
+  onButtonClick() {
+    console.log('button is clicked');
+    
+  }
+  
+  render() {
+    
+    return (
+      <div className="App">
+  
+        <head>
+          <title>Hisham Code retriver</title>
+        </head>
+  
+        <header className="App-header">
+          <p>this page shows any sms messages recived by hisham</p>
+        </header>
+  
+        <body className="App-body">
+          <h1>here will lie all text containers</h1>
+          <smsContainer/>
+          <button onClick = {(e) => this.onButtonClick(e)} >Check for New Code</button>
+        </body>
+  
+        <footer className="App-footer">
+          <p>created by Hisham Aljahbli</p>
+        </footer>
+      </div>
+    );
+    
+  }
+  
 }
 
 export default App;
