97c4855ff26ed1c4dcda07c0aa8e23735fe2ffcd
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-02-15 13:19:02 +0300

    basic initial layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <head>
+        <title>Hisham Code</title>
+      </head>
+
       <header className="App-header">
-        
+        <p>this page shows any sms messages recived by hisham</p>
       </header>
+
+      <body className="App-body">
+        <h1>here will lie all text containers</h1>
+        <smsContainer/>
+      </body>
+
+      <footer className="App-footer">
+        <p>created by Hisham Aljahbli</p>
+      </footer>
     </div>
   );
 }
