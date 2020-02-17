94be2f2e7527985b98c0473a766d09db591a100c
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-02-17 13:31:58 +0300

    Updates

---
+++ precache-manifest.eaec79a10640f1b313b908460f592d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb64572424333578636bd2fe2c154584",
+    "url": "/sms-to-web-react-app/index.html"
+  },
+  {
+    "revision": "a8fbcc7877a3d5d2552d",
+    "url": "/sms-to-web-react-app/static/css/main.334ecf6f.chunk.css"
+  },
+  {
+    "revision": "94992426d7a7a2225f66",
+    "url": "/sms-to-web-react-app/static/js/2.16abde08.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sms-to-web-react-app/static/js/2.16abde08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fbcc7877a3d5d2552d",
+    "url": "/sms-to-web-react-app/static/js/main.51a97a09.chunk.js"
+  },
+  {
+    "revision": "86309789dcfb19a800c5",
+    "url": "/sms-to-web-react-app/static/js/runtime-main.db326b68.js"
+  }
+]);
